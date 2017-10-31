c89fadda40d68f7448f1b1719e4e550f40e6c81a
Yiwei Shen <yshen@talentsoft.com>
2017-10-31 16:13:53 +0100

    talentsoft

---
+++ Algorithmne/AdjacencyList.cs
@@ -0,0 +1,83 @@
+﻿namespace Algorithmne
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class AdjacencyList
+    {
+        public int V;
+        public LinkedList<int>[] adjListArray;
+        bool[] visited;
+
+        public AdjacencyList(int v)
+        {
+            V = v;
+            adjListArray = new LinkedList<int>[V];
+
+            for (var i = 0; i < adjListArray.Length; i++)
+            {
+                adjListArray[i]=new LinkedList<int>();
+            }
+
+            visited = new bool[V];
+        }
+
+        public void AddEdge(int src, int des)
+        {
+            adjListArray[src].AddLast(des);
+            adjListArray[des].AddLast(src);
+        }
+
+        public void DisplayGraphBFS()
+        {
+            bool[] visited = new bool[V];
+
+            Queue<int> queue = new Queue<int>();
+            Console.Write("Please enter the start point : ");
+            int start = int.Parse(Console.ReadLine());
+            queue.Enqueue(start);
+            visited[start] = true;
+
+            while (queue.Count > 0)
+            {
+                int next = queue.Dequeue();
+                Console.Write(next + " ");
+                foreach (int i in adjListArray[next])
+                {
+                    if (visited[i] != true)
+                    {
+                        queue.Enqueue(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+        }
+
+        public void DisplayGraphDFS(int start)
+        {
+            Console.Write(start+" ");
+            visited[start] = true;
+
+            foreach (int i in adjListArray[start])
+            {
+                if (visited[i] != true)
+                {
+                    DisplayGraphDFS(i);
+                    visited[i] = true;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < V; i++)
+            {
+                foreach (int item in adjListArray[i])
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Algorithmne/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adjacency Matrix
+            //AdjacencyMatrix.Generate();
+            //AdjacencyMatrix.Print();
+
+            //Adjacency List
+            //BFS Display
+            //AdjacencyList adjacencyList = new AdjacencyList(5);
+            //adjacencyList.AddEdge(0, 1);
+            //adjacencyList.AddEdge(0, 4);
+            //adjacencyList.AddEdge(1, 2);
+            //adjacencyList.AddEdge(1, 3);
+            //adjacencyList.AddEdge(1, 4);
+            //adjacencyList.AddEdge(2, 3);
+            //adjacencyList.AddEdge(3, 4);
+            ////adjacencyList.Print();
+            ////adjacencyList.DisplayGraphBFS();
+            //Console.Write("Please enter the start point for DFS : ");
+            //int start = int.Parse(Console.ReadLine());
+            //adjacencyList.DisplayGraphDFS(start);
+
+
+
+            Console.Read();
+
+        }
+    }
+}
