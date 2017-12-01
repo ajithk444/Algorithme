01599ca58197a3f4e89fc2521513e18b3670ef8d
Yiwei Shen <yshen@talentsoft.com>
2017-12-01 17:45:28 +0100

    K core Graph

---
+++ Algorithmne/KCore.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithmne
+{
+    public class KCore
+    {
+        public AdjacencyList AdjList { get; set; }
+
+        public KCore(AdjacencyList adjList)
+        {
+            AdjList = adjList;
+        }
+
+        public void DFSUtil(int start, int k)
+        {
+            AdjList.Visited[start] = true;
+            int childrenNum = AdjList.AdjListArray[start].Count;
+
+            for (int i = 0; i < childrenNum; i++)
+            {
+                if (childrenNum < k)
+                {
+                    int item = AdjList.AdjListArray[start].First.Value;
+                    AdjList.AdjListArray[start].Remove(item);
+                    AdjList.AdjListArray[item].Remove(start);
+                    DFSUtil(item, k);
+                }
+                else
+                {
+                    int child = AdjList.AdjListArray[start].ElementAt(i);
+                    if (AdjList.Visited[child] == false)
+                    {
+                        DFSUtil(child, k);
+                    }
+                }
+              
+            }
+        }
+
+        public void Print(int k)
+        {
+            int v = AdjList.V;
+            int startPoint = 0;
+            bool hasKeyCore = false;
+
+            for (int i = 0; i < v; i++)
+            {
+                if (AdjList.AdjListArray[i].Count < k)
+                {
+                    startPoint = i;
+                    break;
+                }
+            }
+
+            DFSUtil(startPoint, k);
+
+            hasKeyCore = AdjList.AdjListArray.Any(s => s.Count >= k);
+
+            if (hasKeyCore)
+            {
+                for (int i = 0; i < AdjList.AdjListArray.Length; i++)
+                {
+                    if (AdjList.AdjListArray[i].Count >= k)
+                    {
+                        Console.Write(i + " => ");
+                        foreach (int m in AdjList.AdjListArray[i])
+                        {
+                            Console.Write(m + " => ");
+                        }
+                    }
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Doesn't have "+ k + "Core Graph");
+            }
+        }
+    }
+}
--- Algorithmne/Program.cs
+++ Algorithmne/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Algorithmne
 {
     public class Program
@@ -132,6 +134,30 @@ namespace Algorithmne
             //{
             //    Console.WriteLine("The value is not found");
             //}
+
+            //Print KCore Graph
+            //Graph 1.0.3
+            AdjacencyList adjacencyList = new AdjacencyList(9);
+            adjacencyList.AddEdge(0, 1);
+            adjacencyList.AddEdge(0, 2);
+            adjacencyList.AddEdge(1, 2);
+            adjacencyList.AddEdge(1, 5);
+            adjacencyList.AddEdge(2, 3);
+            adjacencyList.AddEdge(2, 4);
+            adjacencyList.AddEdge(2, 5);
+            adjacencyList.AddEdge(2, 6);
+            adjacencyList.AddEdge(3, 4);
+            adjacencyList.AddEdge(3, 6);
+            adjacencyList.AddEdge(3, 7);
+            adjacencyList.AddEdge(4, 6);
+            adjacencyList.AddEdge(4, 7);
+            adjacencyList.AddEdge(5, 6);
+            adjacencyList.AddEdge(5, 8);
+            adjacencyList.AddEdge(6, 7);
+            adjacencyList.AddEdge(6, 8);
+
+            KCore kcoreGraph = new KCore(adjacencyList);
+            kcoreGraph.Print(3);
 
             Console.Read();
 
