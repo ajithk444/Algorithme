cd1bdb37eb73a1591cb6891a90c87ced09fe5184
Yiwei Shen <yshen@talentsoft.com>
2018-06-05 11:34:13 +0200

    MDF._2017 Improve the Spirale solution

--- MDF/2017/Spirale.cs
+++ MDF/2017/Spirale.cs
@@ -13,31 +13,27 @@ namespace MDF._2017
             int cX = N / 2;
             int cY = N / 2;
             chars[cX, cY] = '#';
-            cY -= 1;
             int[,] ds = {{ 1,0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }};
-            int dir = 0;
+            int dir = 3;
+            cX += ds[dir, 0];
+            cY += ds[dir, 1];
 
-            if (N > 3)
+            int len = 2;
+            while (true)
             {
-                for (int i = 2; i <= N; i++)
+                if (cX < 0 || cX >= N || cY < 0 || cY >= N) break;
+                dir++;
+                if (dir >= 4) dir %= 4;
+                for (int h = 1; h <= len; h++)
                 {
-                    if (dir >= 4) dir %= 4;
-                    for (int h = 1; h <= i; h++)
-                    {
-                        chars[cX, cY] = '#';
-                        cX += ds[dir, 0];
-                        cY += ds[dir, 1];
-                    }
-                    dir++;
+                    chars[cX, cY] = '#';
+                    cX += ds[dir, 0];
+                    cY += ds[dir, 1];
                 }
-            }
-            else
-            {
-                chars[cX, cY] = '#';
-                chars[cX+1, cY] = '#';
+                len++;
             }
 
-            for (int i = 0; i < N; i++)
+            for(int i = 0; i < N; i++)
             {
                 for (int j = 0; j < N; j++)
                 {
@@ -45,7 +41,6 @@ namespace MDF._2017
                 }
                 Console.WriteLine();
             }
-            Console.Read();
         }
     }
 }
