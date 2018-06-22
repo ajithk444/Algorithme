2375c1a8739f144e23e5270cdf59821a2d6d064f
Yiwei Shen <yshen@talentsoft.com>
2018-06-22 17:49:13 +0200

    Google CodeJam 2018 BathroomStalls

---
+++ Collection/Entities/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection.Entities
+{
+    public class Room : IComparable<Room>
+    {
+        public int Num { get; set; }
+
+        public Room(int num)
+        {
+            Num = num;
+        }
+
+        public int CompareTo(Room other)
+        {
+            return other.Num - this.Num;
+        }
+
+        public override string ToString()
+        {
+            return Num.ToString();
+        }
+    }
+}
--- Collection/Program.cs
+++ Collection/Program.cs
@@ -12,18 +12,31 @@ namespace Collection
         static void Main(string[] args)
         {
             // Priority Queue Test
-            PriorityQueue<Employee> priorityQueue = new PriorityQueue<Employee>();
-            priorityQueue.Enqueue(new Employee("Litao", 100));
-            priorityQueue.Enqueue(new Employee("JingLi", 12));
-            priorityQueue.Enqueue(new Employee("Leonard", 8));
-            priorityQueue.Enqueue(new Employee("Shen", 3));
-            priorityQueue.Enqueue(new Employee("Vincent", 13));
-            priorityQueue.Enqueue(new Employee("Xinlong", 11));
-            Console.WriteLine(priorityQueue);
-            priorityQueue.Dequeue();
-            Console.WriteLine(priorityQueue);
-            priorityQueue.Enqueue(new Employee("Huangshen", 2));
-            Console.WriteLine(priorityQueue);
+            //PriorityQueue<Employee> priorityQueue = new PriorityQueue<Employee>();
+            //priorityQueue.Enqueue(new Employee("Litao", 100));
+            //priorityQueue.Enqueue(new Employee("JingLi", 12));
+            //priorityQueue.Enqueue(new Employee("Leonard", 8));
+            //priorityQueue.Enqueue(new Employee("Shen", 3));
+            //priorityQueue.Enqueue(new Employee("Vincent", 13));
+            //priorityQueue.Enqueue(new Employee("Xinlong", 11));
+            //Console.WriteLine(priorityQueue);
+            //priorityQueue.Dequeue();
+            //Console.WriteLine(priorityQueue);
+            //priorityQueue.Enqueue(new Employee("Huangshen", 2));
+            //Console.WriteLine(priorityQueue);
+
+            PriorityQueue<Room> queue = new PriorityQueue<Room>();
+            queue.Enqueue(new Room(1000));
+            int personNum = 4;
+            for (int i = 0; i < personNum; i++)
+            {
+                var room = queue.Dequeue();
+                queue.Enqueue(new Room((room.Num - 1) / 2));
+                queue.Enqueue(new Room((room.Num) / 2));
+            }
+
+            Console.WriteLine(queue);
+
             Console.Read();
         }
     }
