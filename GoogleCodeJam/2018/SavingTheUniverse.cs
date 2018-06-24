4af3d16657b1566d1a30846aa092c53ed6d142c3
yiweishen1988 <yiweishen1988@gmail.com>
2018-06-24 22:28:16 +0200

    Code jam SavingTheUniverse

--- GoogleCodeJam/2018/SavingTheUniverse.cs
+++ GoogleCodeJam/2018/SavingTheUniverse.cs
@@ -5,17 +5,17 @@ namespace GoogleCodeJam
 {
     public class SavingTheUniverse
     {
-        public static void Start()
+        public static void Main()
         {
             int t = Convert.ToInt32(Console.ReadLine());
 
-            int[] D = new int[t];
+            long[] D = new long[t];
             string[] P = new string[t];
 
             for (int i = 0; i < t; i++)
             {
                 var strs = Console.ReadLine().Split(' ');
-                D[i] = Convert.ToInt32(strs[0]);
+                D[i] = Convert.ToInt64(strs[0]);
                 P[i] = strs[1];
             }
 
@@ -23,7 +23,8 @@ namespace GoogleCodeJam
             {
                 string str = P[i].TrimEnd('C');
                 int[] nums = new int[str.Length];
-                for (int j = 0, h=0; j < str.Length; j++)
+
+                for (int j = 0, h = 0; j < str.Length; j++)
                 {
                     if (str[j] == 'C')
                     {
@@ -35,23 +36,25 @@ namespace GoogleCodeJam
                     }
                 }
 
-                int minSum = nums.Sum();
+                long minSum = nums.Sum();
                 if(D[i] < minSum)
                 {
                     Output(i + 1, 0, false);
                 }
                 else
                 {
-                    int step = 0;
-                    int curIndex = nums.LastOrDefault(s => s!=0);
-                    int sum = (int)nums.Select((s, index) => s * Math.Pow(2, index)).Sum();
+                    long step = 0;
+                    var curEntity = nums.Select((val, idx) => new {val=val, idx=idx}).Where(s => s.val!=0).LastOrDefault();
+                    int curIndex = curEntity == null ? 0 : curEntity.idx;
+      
+                    long sum = (long)nums.Select((s, index) => s * Math.Pow(2, index)).Sum();
                     while (sum > D[i])
                     {
                         if (nums[curIndex] != 0)
                         {
                             nums[curIndex]--;
                             nums[curIndex-1]++;
-                            sum =sum - (int)Math.Pow(2, curIndex) + (int)Math.Pow(2, curIndex-1);
+                            sum =sum - (long)Math.Pow(2, curIndex-1);
                             step++;
                         }
                         else
@@ -62,10 +65,12 @@ namespace GoogleCodeJam
 
                     Output(i + 1, step, true);
                 }
+               
             }
+            Console.Read();
         }
 
-        public static void Output(int caseNum, int result, bool isPossible)
+        public static void Output(int caseNum, long result, bool isPossible)
         {
             Console.Write("Case #" + caseNum + ": ");
             if (isPossible)
