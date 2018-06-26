85a7b06e011cf38da28112b8f10ecdc46f5b2b76
Yiwei Shen <yshen@talentsoft.com>
2018-06-26 15:52:22 +0200

    Calculate the Polygon area

---
+++ Math/Geometric/Polygon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Maths.Geometric
+{
+    public class Polygon
+    {
+        /// <summary>
+        /// We can compute area of a polygon using Shoelace formula.
+        /// </summary>
+        public static double GetPolygonArea(double[] X, double[] Y, int n)
+        {
+
+            // Initialze area
+            double area = 0.0;
+
+            // Calculate value of shoelace formula
+            int j = n-1;
+
+            for (int i = 0; i < n; i++)
+            {
+                area += (X[j] + X[i]) * (Y[j] - Y[i]);
+
+                // j is previous vertex to i
+                j = i;
+            }
+
+            // Return absolute value
+            return Math.Abs(area/2);
+        }
+    }
+}
---
+++ Math/Program.cs
@@ -0,0 +1,23 @@
+﻿using Maths.Geometric;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polygon area
+            double[] x = { 1, 3, 3, 1 };
+            double[] y = { 2, 5, -5, -3};
+
+            Console.WriteLine(Polygon.GetPolygonArea(x, y, x.Count()));
+
+            Console.Read();
+        }
+    }
+}
