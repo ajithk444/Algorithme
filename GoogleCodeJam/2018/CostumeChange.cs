e18a1105bc85048fccff584b3f7256c1f086543a
Yiwei Shen <yshen@talentsoft.com>
2018-09-06 15:15:37 +0200

    GoogleCodeJam 2018 CostumeChange solution

--- GoogleCodeJam/2018/CostumeChange.cs
+++ GoogleCodeJam/2018/CostumeChange.cs
@@ -10,7 +10,7 @@ namespace CodeJam.Model
         public static int N;
         public static int[][] Nums;
 
-        public static void Start()
+        public static void Main()
         {
 #if true
             System.IO.StreamReader input = new System.IO.StreamReader(@"test\CostumeChange.txt");
@@ -27,36 +27,80 @@ namespace CodeJam.Model
                     Nums[j] = input.ReadLine().Split(' ').Select(int.Parse).ToArray();
                 }
 
-                Solve();
+                Output(i + 1, Solve());
             }
 
-            //Console.Read();
+            Console.Read();
         }
-
-        public static void Solve()
+        public static bool BMP(int[,] bmp, int u, int[] match, bool[] seen)
         {
-            int[,] Rs = new int[N, 2*N+1];
-            int[,] Cs = new int[N, 2 * N + 1];
             for (int i = 0; i < N; i++)
             {
-                for (int j = 0; j < N; j++)
+                if(bmp[u, i] == 1 && !seen[i])
                 {
-                    Rs[i, Nums[i][j]]++;
-                    Cs[j, Nums[i][j]]++;
+                    seen[i] = true;
+                    if (match[i] == -1 || BMP(bmp, match[i], match, seen))
+                    {
+                        match[i] = u;
+                        return true;
+                    }
                 }
             }
 
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    Rs[i, Nums[i][j]]++;
-                    Cs[j, Nums[i][j]]++;
-                }
-            }
+            return false;
         }
 
-        public static void Output(int caseNum, string result)
+        public static int GetMaximumBitPartite(int[,] bmp)
+        {
+            int counter = 0;
+            int[] match = new int[N];
+            for (int j = 0; j < N; j++)
+            {
+                match[j] = -1;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                bool[] seen = new bool[N];
+                if (BMP(bmp, i, match, seen))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public static int Solve()
+        {
+            int counter = 0; 
+            int[][,] bmp = new int[2 * N + 1][,];
+            for (int i = 0; i < 2 * N + 1; i++)
+            {
+                bmp[i] = new int[N, N];
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    bmp[Nums[i][j]+N][i,j] = 1;
+                }
+            }
+
+            for (int i = -N; i <= N; i++)
+            {
+                if (i == 0) continue;
+                counter += GetMaximumBitPartite(bmp[i+N]);
+            }
+            
+
+            return N * N - counter;
+        }
+           
+            
+
+        public static void Output(int caseNum, int result)
         {
             Console.Write("Case #" + caseNum + ": " + result);
             Console.WriteLine();
